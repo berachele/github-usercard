37f1acaf8271bd1d27093a724fb404e0dcf556c3
Rachele Edwards <dot.com1472@gmail.com>
2020-03-12 16:22:13 -0600

    still stuck on last step, pari programming in the works

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -9,7 +9,7 @@ axios.get('https://api.github.com/users/berachele')
   cards.append(createCard(response.data))
 })
 .catch(error => {
-  console.log('THE DATA WAS NOT RETURNED', error)
+  console.log('THE DATA WAS NOT RETURNED:', error)
 })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -23,19 +23,34 @@ axios.get('https://api.github.com/users/berachele')
 */
 const cards = document.querySelector('.cards')
 
-
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
           at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
+          Individual strings to the friendsArray below.✅
           
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'JLC6290',
+  'AlexJoeb',
+  'ChristianCorby',
+  'Pinefoot',
+  'HunterStevens'
+]; //how do I pass in my followersArray??
 
+axios.get('https://api.github.com/users/berachele/followers')
+.then(response => {
+  console.log(response)
+  response.data.forEach(item => {
+    cards.append(createCard(response.data))
+  })
+})
+.catch(error => {
+  console.log('FRIENDS DATA ERROR:', error)
+})
 /* Step 3: Create a function that accepts a single object as its only argument, ✅
           Using DOM methods and properties, create a component that will return the following DOM element: ✅
 
